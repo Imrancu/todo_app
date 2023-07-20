9aa600442958a2491e0ed13fff968e7e1cfa8e62
Imrancu <mdimran3772@gmail.com>
2023-07-21 02:44:03 +0600

    completed my todo app

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,8 +1,60 @@
-export default function AddTopic() {
-    return <form className="flex flex-col gap-3">
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
-        <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
-        <button className="bg-purple-950 font-bold text-white py-2 px-5 w-fit rounded">Add Topic</button>
+"use client";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if(!title || !description) {
+        alert("Title and Description are required.")
+        return
+    }
+
+    try {
+        const res = await fetch("http://localhost:3000/api/topics", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ title, description })
+        })
+        if(res.ok){
+            router.refresh()
+            router.push("/")
+        }
+        else {
+            throw new Error("Failed to create a topic")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+      <button type="submit" className="bg-purple-950 font-bold text-white py-2 px-5 w-fit rounded">
+        Add Topic
+      </button>
     </form>
+  );
 }
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,7 +1,56 @@
-export default function EditTopicForm(){
-    return <form className="flex flex-col gap-3">
-    <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
-    <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
-    <button className="bg-purple-950 font-bold text-white py-2 px-5 w-fit rounded">Update Topic</button>
-</form>
+"use client"
+
+import { useRouter } from "next/navigation";
+import {useState } from "react";
+
+const EditTopicForm = ({ id, title, description }) => {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({newTitle, newDescription })
+        })
+        if(!res.ok){
+            throw new Error("Failed to update topic")
+        }
+        router.refresh()
+        router.push("/")
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+      <button type="submit" className="bg-purple-950 font-bold text-white py-2 px-5 w-fit rounded">
+        Update Topic
+      </button>
+    </form>
+  )
 }
+
+
+export default EditTopicForm
